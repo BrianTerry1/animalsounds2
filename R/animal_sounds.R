b0a1918925252bca89c6c2a87050d2fb1dc2354e
BrianTerry1 <56925848+BrianTerry1@users.noreply.github.com>
2023-06-20 16:57:55 +0100

    Add links in documentation.
    Update error messaging.

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -3,7 +3,9 @@
 #' This function takes two character strings. One for
 #' the animal you want to represent and one for the
 #' sound it makes. There's an easter egg for entering
-#' "fox".
+#' "fox". We use the `paste0` function in our script.
+#' To find out more, visit the [documentation][base::paste0()].
+#' We have not used [basemodels::dummy_classifier()].
 #'
 #' @param animal
 #' @param sound
@@ -12,10 +14,21 @@
 #' @export
 #'
 #' @examples
+#'
+#' @import rlang
+#' @import cli
+#'
 
 animal_sounds <- function(animal, sound) {
-  stopifnot(is.character(animal) & length(animal) == 1)
-  stopifnot(is.character(sound) & length(sound) == 1)
+  if (!rlang::is_character(animal, n = 1)){
+    cli::cli_abort("`animal` must be a single string!")
+  }
+  if (!rlang::is_character(sound, n = 1)){
+    cli::cli_abort("`sound` must be a single string!")
+    }
+
+  #stopifnot(is.character(animal) & length(animal) == 1)
+  #stopifnot(is.character(sound) & length(sound) == 1)
   if (animal == "fox"){
     paste("What does the fox say?")
   } else {
