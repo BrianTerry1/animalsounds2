760d22f1da76fb8e46693fc0e015b67af8b707b9
BrianTerry1 <56925848+BrianTerry1@users.noreply.github.com>
2023-06-20 16:05:56 +0100

    Added documentation.

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,18 @@
+#' Animal Sounds Function - all your favourite animals!
+#'
+#' This function takes two character strings. One for
+#' the animal you want to represent and one for the
+#' sound it makes. There's an easter egg for entering
+#' "fox".
+#'
+#' @param animal
+#' @param sound
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
