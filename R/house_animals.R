beebb43b7336c1a258a4e771d2f736e81e2bc43b
BrianTerry1 <56925848+BrianTerry1@users.noreply.github.com>
2023-06-21 15:14:19 +0100

    Add farm animals data set.
    Begin working on web page documentation.

---
+++ R/house_animals.R
@@ -0,0 +1,11 @@
+#' Data Frame of House Animals with their Sounds
+#'
+#' A dataset of animals (currently cats and dogs) from the home
+#' and their sounds.
+#'
+#' @format A data frame with 2 rows and 2 variables:
+#' \describe{
+#'   \item{animal}{The animal as a character string.}
+#'   \item{sound}{The corresponding sound.}
+#' }
+"house_animals"
---
+++ data-raw/house_animals.R
@@ -0,0 +1,4 @@
+
+house_animals <- data.frame(animal = c("cat", "dog"), sound = c("meow", "woof"))
+
+usethis::use_data(house_animals, overwrite = TRUE)
